53eb840e826e7fdf7649f30857be59384d68eab3
Sönke Liebau <soenke.liebau@stackable.tech>
2023-06-29 21:57:28 +0200

    Added sql definition for materialized view roxy.

---
+++ trino/views/roxy_persistent.sql
@@ -0,0 +1,14 @@
+CREATE MATERIALIZED VIEW lakehouse.smart_city.roxy_persistent AS
+WITH
+  json AS (
+   SELECT CAST(json_parse(JSON_QUERY(data FORMAT JSON, 'lax $.features[*]' WITH UNCONDITIONAL ARRAY WRAPPER NULL ON EMPTY NULL ON ERROR)) AS array(ROW(properties ROW(id varchar, reserved varchar), geometry ROW(type varchar, coordinates array(double))))) roxies
+   FROM
+     storage.raw."https://geoportal.freiburg.de/wfs/gdm_sharing/gdm_sharing?request=GetFeature&service=WFS&version=1.1.0&typeName=:roxy&outputFormat=application%2Fjson%3B%20subtype%3Dgeojson%3B%20charset%3Dutf-8&SRSNAME=urn:ogc:def:crs:EPSG::4326"
+)
+SELECT properties.id           id
+     , properties.reserved     reserved
+     , geometry.coordinates[2] latitude
+     , geometry.coordinates[1] longitude
+FROM
+    (json
+        CROSS JOIN UNNEST(roxies));
