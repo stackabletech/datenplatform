908b370a629dde1d2204adbd691a1dfc4781ccf9
Sebastian Bernauer <sebastian.bernauer@stackable.de>
2023-07-14 12:42:56 +0200

    fixup

--- airflow_dags/table_maintenance.py
+++ airflow_dags/table_maintenance.py
@@ -25,6 +25,8 @@ with models.DAG(
         )
         TrinoOperator(
             task_id=f"trino_maintenance_expire_snapshots_{table}",
-            sql=f"alter table {table} execute expire_snapshots(retention_threshold => '2d')",
+            # We need to set at least 7 days because of
+            # io.trino.spi.TrinoException: Retention specified (2.00d) is shorter than the minimum retention configured in the system (7.00d). Minimum retention can be changed with iceberg.expire_snapshots.min-retention configuration property or iceberg.expire_snapshots_min_retention session property
+            sql=f"alter table {table} execute expire_snapshots(retention_threshold => '7d')",
             handler=list,
         )
