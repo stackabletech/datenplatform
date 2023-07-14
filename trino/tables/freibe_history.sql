239ea491c1419a14f9af577f18a8e35495bc2b1d
Sebastian Bernauer <sebastian.bernauer@stackable.de>
2023-07-14 12:21:08 +0200

    Improve freibe

--- trino/tables/freibe_history.sql
+++ trino/tables/freibe_history.sql
@@ -1,8 +1,8 @@
-CREATE TABLE lakehouse.platform.freibe_history
-(
-    id            varchar,
-    fuellevel     bigint,
-    vehicletypeid varchar,
-    latitude      double,
-    longitude     double
+CREATE TABLE lakehouse.platform.freibe_history (
+    id              varchar,
+    ts              timestamp(6),
+    fuel_level      bigint,
+    vehicle_type_id varchar,
+    latitude        double,
+    longitude       double
 );
